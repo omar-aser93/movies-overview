1d7540a80fcf5a2c76581bbad0617232b1326763
omar-mohamed <omaraser93@yahoo.com>
2021-10-28 14:04:39 +0200

    Updates

---
+++ precache-manifest.02146f6d0b2c8f72def24bd6076c4c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2a2d3e1f3df9f980f321b97c4d818b",
+    "url": "/movies-overview/index.html"
+  },
+  {
+    "revision": "9a75c69d7ed6a3b53f79",
+    "url": "/movies-overview/static/css/main.1695b93d.chunk.css"
+  },
+  {
+    "revision": "8d71c70559220820381b",
+    "url": "/movies-overview/static/js/2.6d7419ef.chunk.js"
+  },
+  {
+    "revision": "4844a3cf5a99d1261bd981ecae8befeb",
+    "url": "/movies-overview/static/js/2.6d7419ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a75c69d7ed6a3b53f79",
+    "url": "/movies-overview/static/js/main.a9eb5aa1.chunk.js"
+  },
+  {
+    "revision": "858820c323de1c2f6f78",
+    "url": "/movies-overview/static/js/runtime-main.1b4fe869.js"
+  }
+]);
